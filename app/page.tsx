49ec70eea2fda56469c84280cdcaf60055523502
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-02-05 17:41:51 +0100

    Implement API call.

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,36 @@
-import Image from "next/image";
+'use client'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import dataFetch from "@/dataFetch";
 
 export default function Home() {
+  const [products, setProducts] = useState<{}[]>([]);
+  const fetchProducts = async () => {
+      try {
+        const response = await dataFetch.get('', {
+          params: {
+            MarketId: 9,
+            PriceListId: 19,
+            LanguageCode: 'en',
+            Skip: 0,
+            'Where.IncludeFacets': false,
+            Take: 10,
+            CategorySlug: 'all'
+          }
+        });
+        setProducts(response.data);
+
+      } catch (error) {
+
+      }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  console.log({products});
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Eton Shirts</div>
--- dataFetch.ts
+++ dataFetch.ts
@@ -1,5 +1,7 @@
 import axios from 'axios';
 
 const dataFetch = axios.create({
-    baseURL: 'https://staging-api.etonshirts.com/V1/Retail/Catalog/Category?MarketId=9&PriceListId=19&LanguageCode=en&Skip=0&Where.IncludeFacets=false&Take=10&CategorySlug=all'
+    baseURL: 'https://staging-api.etonshirts.com/V1/Retail/Catalog/Category'
 })
+
+export default dataFetch;
