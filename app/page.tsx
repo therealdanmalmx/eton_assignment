9432dd9f87d990d228f00ff051d5a5674563cf59
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-02-05 17:57:30 +0100

    Install react-hot-toast. Crete basic implementation.

--- app/page.tsx
+++ app/page.tsx
@@ -2,38 +2,46 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 import dataFetch from "@/dataFetch";
+import toast, { Toaster } from 'react-hot-toast';
+
 
 export default function Home() {
   const [products, setProducts] = useState<{}[]>([]);
   const fetchProducts = async () => {
-      try {
-        const response = await dataFetch.get('', {
-          params: {
-            MarketId: 9,
-            PriceListId: 19,
-            LanguageCode: 'en',
-            Skip: 0,
-            'Where.IncludeFacets': false,
-            Take: 10,
-            CategorySlug: 'all'
-          }
-        });
-        setProducts(response.data);
+    try {
+      const response = await dataFetch.get('', {
+        params: {
+          MarketId: 9,
+          PriceListId: 19,
+          LanguageCode: 'en',
+          Skip: 0,
+          'Where.IncludeFacets': false,
+          Take: 10,
+          CategorySlug: 'all'
+        }
+      });
+      setProducts(response.data);
 
-      } catch (error) {
+    } catch (error) {
+        toast.error('Something went wrong', {
+            duration: 4000,
+            position: 'bottom-center',
+            className: 'cursor-pointer',
+        });
 
       }
-  };
+    };
 
-  useEffect(() => {
-    fetchProducts();
-  }, []);
+    useEffect(() => {
+      fetchProducts();
+    }, []);
 
-  console.log({products});
+    console.log({products});
 
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div>Eton Shirts</div>
+      <Toaster />
 
     </main>
   );
--- dataFetch.ts
+++ dataFetch.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const dataFetch = axios.create({
-    baseURL: 'https://staging-api.etonshirts.com/V1/Retail/Catalog/Category'
+    baseURL: 'https://staging-api.etonshirts.com/V1/Retail/Catalog/Categor'
 })
 
 export default dataFetch;
