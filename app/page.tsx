8ad565a2e5beddf058e191255b42278113b5a668
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-02-06 23:06:32 +0100

    Add types

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,38 @@
 'use client'
 import dataFetch from "@/dataFetch";
-import Link from "next/link";
 import Image from 'next/image';
 import { notFound } from "next/navigation";
 import { useEffect, useState } from "react";
 import toast, { Toaster } from 'react-hot-toast';
 
+interface RetailImage {
+  mediaKey: string;
+}
+
+interface ProductVariant {
+  retailImages: {
+    thumbnail: RetailImage;
+    gallery: RetailImage[];
+  };
+  price: {
+    formattedPriceBeforeDiscount: string;
+  };
+}
+interface Product {
+  id: string | number;
+  name: string;
+  uri: string;
+  productVariants: ProductVariant[];
+}
+
 export default function Home() {
   const baseUrl = 'https://staging-api.etonshirts.com';
-  const [products, setProducts] = useState<{}[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
   const fetchProducts = async () => {
     try {
-      const response = await dataFetch.get('/V1/Retail/Catalog/Category?MarketId=9&PriceListId=19&LanguageCode=en&Skip=0&Where.IncludeFacets=false&Take=10&CategorySlug=all');
-      setProducts(response.data.displays);
+      const res = await dataFetch.get('/V1/Retail/Catalog/Category?MarketId=9&PriceListId=19&LanguageCode=en&Skip=0&Where.IncludeFacets=false&Take=10&CategorySlug=all');
+      setProducts(res.data.displays);
 
     } catch (error) {
         toast.error(
@@ -38,25 +57,27 @@ export default function Home() {
       toast.error('No products found');
       return notFound();
     }
-
-    const productList = products;
     console.log({products});
 
     return (
       <main>
         <div className="text-3xl font-bold text-center my-12">Eton</div>
         <div className="grid grid-cols-2 gap-1 lg:grid-cols-3 lg:gap-2">
-          {products.map((product: any) => (
+          {products.map((product: Product) => (
             <div key={product.id} className="overflow-hidden">
               <div className="relative min-h-[200px] w-full">
-                <img
-                  className="w-full h-full object-cover transition-all duration-500 cursor-pointer hover:opacity-0"
+                <Image
                   src={`${baseUrl}/v1/retail/image/1080/bynder/${product.productVariants[0].retailImages.thumbnail.mediaKey}/${product.uri}.webp`}
                   alt={product.name}
-                />
+                  width={400}
+                  height={500}
+                  className="w-full h-full object-cover transition-all duration-500 cursor-pointer hover:opacity-0"
+                  />
                 {product.productVariants[0].retailImages.gallery.map((image: any) => (
-                  <img
+                  <Image
                     key={image.mediaKey}
+                    width={400}
+                    height={500}
                     className="absolute inset-0 w-full h-full object-cover opacity-0 transition-all duration-500 cursor-pointer hover:opacity-100"
                     src={`${baseUrl}/v1/retail/image/1080/bynder/${image.mediaKey}/${product.uri}.webp`}
                     alt={product.name}
--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        remotePatterns: [
-          {
-            protocol: 'https',
-            hostname: 'staging-api.etonshirts.com',
-            port: '',
-            pathname: '/v1/retail/image/1080/bynder',
-          },
-          {
-            protocol: 'https',
-            hostname: 'pixabay.com',
-            port: '',
-            pathname: '',
-          },
-        ],
+        domains: ['staging-api.etonshirts.com'],
       },
 };
 
