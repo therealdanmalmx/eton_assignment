4e7f9f4f96dbdbee97324cdff02e4454311d6475
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2024-02-06 23:34:51 +0100

    use remotePatterns for images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['staging-api.etonshirts.com'],
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'staging-api.etonshirts.com',
+            port: '',
+            pathname: '/v1/retail/image/',
+          },
+        ],
       },
 };
 
